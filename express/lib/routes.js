d257a55b8c8274a0dcf898d3c133c03ca0e08c37
Chongguang <chongguang.liu@insa-lyon.fr>
2015-12-16 13:42:47 +0100

    init project

---
+++ express/lib/routes.js
@@ -0,0 +1,21 @@
+exports.order = function order(req, res, next) {
+  // TODO implement from here
+  res.send(200);
+}
+
+exports.feedback = function feedback(req, res, next) {
+  // FIXME ignored for now
+  next();
+}
+
+exports.products = function products(req, res, next) {
+  // FIXME ignored for now
+  res.send(200);
+
+}
+
+exports.tests = function products(req, res, next) {
+  // FIXME ignored for now
+  res.send(200);
+
+}
---
+++ express/server.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var routes = require('./lib/routes');
+
+module.exports = Server;
+
+function Server (doLogRequests) {
+  var app = express();
+  app.set('json spaces', 2);
+
+  if (doLogRequests) {
+    app.use(morgan('dev'));
+  }
+  app.use(bodyParser.json());
+
+  app.get('/status', function (req, res) {
+    res.json({up: true})
+  })
+
+
+
+  
+  app.get('/test', function (req, res, next) {
+    routes.tests(req, res, next);
+  });
+
+
+  var server = http.createServer(app);
+  server.start = server.listen.bind(server, process.env.PORT || 3000);
+  server.stop = server.close.bind(server);
+  return server;
+}
+
+
+if (!module.parent) {
+  var server = new Server(true);
+  server.start(function () {
+    console.log('server listening on port', server.address().port);
+  });
+}
