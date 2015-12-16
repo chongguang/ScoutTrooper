e10da1863aa9370155ebcffcb96b59f50befb316
Chongguang <chongguang.liu@insa-lyon.fr>
2015-12-16 14:09:06 +0100

    delete useless routes

--- express/lib/routes.js
+++ express/lib/routes.js
@@ -1,8 +1,3 @@
-exports.feedback = function feedback(req, res, next) {
-  // FIXME ignored for now
-  next();
-}
-
 exports.tests = function products(req, res, next) {
   // FIXME ignored for now
   res.send(200);
